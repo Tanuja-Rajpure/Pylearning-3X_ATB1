9a1277793e2547c65b984318ccbc5a8a1dbb13d3
Tanuja Rajpure <tdkamthe@gmail.com>
2024-06-05 08:29:39 +0530

    Variables and Data type

--- Exercise 03062024/Lab 001.py
+++ Exercise 03062024/Lab 001.py
@@ -1,2 +1,6 @@
 print("Hello World")
 print("My first python code.")
+
+
+self, *args, sep=' ', end='\n', file=None
+#*arg- unlimited no of arguments
---
+++ Exercise 05062024/Lab 003.py
@@ -0,0 +1,4 @@
+print("Hello", "world", 123, True, sep="-")
+print("Hello", "world", 123, True, sep="-")
+
+print(type("Tanuja"))

118c520e588662693e93548efba773c1367fdfe1
Tanuja Rajpure <tdkamthe@gmail.com>
2024-06-30 20:46:44 +0530

    Recursion

--- Practice 24062024/Recursion.py
+++ Practice 24062024/Recursion.py
@@ -7,9 +7,23 @@
 
 # Factorial programme
 
+#************** Programme 1 **************
+n = int(input("Enter value of n = "))
+factorial = 1
+# result = math.factorial(5)
+# print(result)
+
+for i in range(1, n + 1):  # n+1 used as in range we always take range till n-1, so to get range till n, we need to give n+1
+    factorial = factorial * i
+
+print("Factorial is:", factorial)
+
+#*******************************
+
+
+#************** Programme 2 **************
+
 n = int(input("Enter number to calculate factorial: "))
-
-
 def factorial(n):
     # Base Case - If n=0 / 1 this is min we can go till max of n
     if n == 0 or n == 1:

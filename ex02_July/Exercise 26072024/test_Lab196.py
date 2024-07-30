0c1c31bdc7374c3e0b642ef44c4b068041d2ca9a
Tanuja Rajpure <tdkamthe@gmail.com>
2024-07-30 21:34:44 +0530

    Pytest Fixtures

---
+++ ex02_July/Exercise 26072024/test_Lab196.py
@@ -0,0 +1,9 @@
+# We can write all test cases under parent class as well
+
+class Test_CRUD(object):
+    def test_update_1(self):
+        assert True == True
+
+    def test_update_1(self):
+        assert True == False
+
